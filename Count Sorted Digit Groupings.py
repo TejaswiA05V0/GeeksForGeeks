cbedd312876f3f30b8c0ada558202161ca72285b
TejaswiA05V0 <tejaswidts6@gmail.com>
2026-05-29 20:26:00 +0530

    Create Count Sorted Digit Groupings.py

---
+++ Count Sorted Digit Groupings.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+class Solution:
+    def validGroups(self, s):
+        # code here
+        n=len(s)
+        @lru_cache(None)
+        def dfs(idx,prev_sum):
+            if idx==n:
+                return 1 
+            
+            ans=0
+            cur_sum=0
+            for i in range(idx,n):
+                cur_sum+=int(s[i])
+                if cur_sum>=prev_sum:
+                    ans+=dfs(i+1,cur_sum)
+            return ans
+        
+        return dfs(0,0)
