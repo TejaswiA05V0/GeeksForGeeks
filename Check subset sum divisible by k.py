7c923c526f20c9d8d1ad99dc9e4ba7aba91e3927
Gandhamoju Tejaswi <tejaswidts6@gmail.com>
2026-07-02 15:30:24 +0530

    Implement subset sum divisible by k algorithm

---
+++ Check subset sum divisible by k.py
@@ -0,0 +1,14 @@
+class Solution:
+    def divisibleByK(self, arr, k):
+        # code here
+        dp = [[False] * k for _ in range(len(arr) + 1)]
+        dp[0][0] = True
+        for i, v in enumerate(arr):
+            for j in range(k):
+                if dp[i][j]:
+                    dp[i+1][j] = True
+                    rem = (j + v) % k
+                    if rem == 0:
+                        return True
+                    dp[i+1][rem] = True
+        return False
